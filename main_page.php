72b6d927534d553e3db58af455262463190e30d1
ViktM <viktoria.mezei0708@gmail.com>
2018-05-12 20:51:01 +0100

    main page idea

---
+++ main_page.php
@@ -0,0 +1,64 @@
+
+
+
+
+
+
+<!DOCTYPE html>
+
+<html>
+    <head>
+        <meta charset="UTF-8">
+        
+        
+        
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
+        <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.0.8/css/solid.css">
+        <link href="https://fonts.googleapis.com/css?family=Pacifico" rel="stylesheet">
+        <link rel="stylesheet" type="text/css" href="main_page_style.css">
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+        <title>Sexy Salad - Main Page</title>
+    </head>
+    <body>
+        
+        <header>
+                <div class="jumbotron jumbotron-fluid">
+        <h1 id="sexy_salads">Sexy Salads</h1>
+                </div>
+            
+        </header>
+        
+        <div class="topnav" id="myTopnav">
+            
+            
+  <a href="#home" class="active">Home</a>
+  <a href="#recipes">Recipes</a>
+  <a href="#contact">Contact</a>
+  <a href="#about">About</a>
+
+    
+ 
+        
+  <a href="#login">Login</a>
+  <a href="#register">Register</a>
+  <a href="#my_fav">My Favourites</a>
+  <a href="#about">Logout</a>
+  <a href="https://www.facebook.com"><img src="facebook.jpg" height="40" width="60" alt="Facebook"></a>
+  <a href="https://www.twitter.com"><img src="twitter.jpg" height="40" width="60" alt="Twitter"></a>
+  <a href="https://www.instagram.com"><img src="instagram.jpg" height="40" width="60" alt="Instagram"></a>
+  <a href="https://www.pinterest.com"><img src="pinterest.jpg" height="40" width="60" alt="Pinterest"></a>
+  <a href="https://www.whatsapp.com"><img src="whatsapp.jpg" height="40" width="60" alt="WhatsApp"></a>
+  
+  <a href="javascript:void(0);" class="icon" onclick="myFunction()">
+      
+ 
+    
+  </a>
+</div>
+        
+        
+        <?php
+        // put your code here
+        ?>
+    </body>
+</html>
