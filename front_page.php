9ed05982e8c6313e588edbce86ce1f4c18c71d90
johasago <jo@gowithjo.online>
2018-05-13 20:03:26 +0100

    front page ideas

---
+++ front_page.php
@@ -0,0 +1,205 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <title>Sexy Salads</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  
+    <link href="https://fonts.googleapis.com/css?family=Averia+Serif+Libre|Noto+Serif|Tangerine" rel="stylesheet" />
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/meyer-reset/2.0/reset.min.css">
+
+    <!-- Add icon library -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css" />
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+  <link rel="stylesheet" type="text/css" href="styles.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+</head>
+  <body>
+      <div class="header">
+          <div id="share-buttons">
+
+  <!-- Email -->
+    <a href="mailto:?Subject=Simple Share Buttons&amp;Body=I%20saw%20this%20and%20thought%20of%20you!%20 https://simplesharebuttons.com">
+        <img src="https://simplesharebuttons.com/images/somacro/email.png" alt="Email" />
+    </a>
+ <!-- Print -->
+    <a href="javascript:;" onclick="window.print()">
+        <img src="https://simplesharebuttons.com/images/somacro/print.png" alt="Print" />
+    </a>
+    <!-- Facebook -->
+    <a href="http://www.facebook.com/sharer.php?u=https://simplesharebuttons.com" target="_blank">
+        <img src="https://simplesharebuttons.com/images/somacro/facebook.png" alt="Facebook" />
+    </a>
+    <!-- Pinterest -->
+    <a href="javascript:void((function()%7Bvar%20e=document.createElement('script');e.setAttribute('type','text/javascript');e.setAttribute('charset','UTF-8');e.setAttribute('src','http://assets.pinterest.com/js/pinmarklet.js?r='+Math.random()*99999999);document.body.appendChild(e)%7D)());">
+        <img src="https://simplesharebuttons.com/images/somacro/pinterest.png" alt="Pinterest" />
+    </a>
+    
+     <!-- Twitter -->
+    <a href="https://twitter.com/share?url=https://simplesharebuttons.com&amp;text=Simple%20Share%20Buttons&amp;hashtags=simplesharebuttons" target="_blank">
+        <img src="https://simplesharebuttons.com/images/somacro/twitter.png" alt="Twitter" />
+    </a>
+    
+</div>
+      </div>
+      
+<div class="topnav">
+  <div class="col-md-10">
+			<ul>
+			  <li><a class="active" href="index.php">Home</a></li>
+                          <li class="dropdown"><a href=#recipes>Recipes</a>
+  <div class="dropdown-content">
+    <a href="#">Vegetarian</a>
+    <a href="#">Meat</a>
+    <a href="#">Pasta</a>
+    <a href="#">
+        
+  </div>
+                          </li>
+                          
+                          
+                          <li>
+				<a class="logo" href="index.php"><h1>Sexy Salads</h1></a>
+			</li>
+			  
+                          <li><a href="#contact">Contact</a></li>
+			  <li><a href="#about">About</a></li>
+			</ul>
+                        </div>
+</div>
+
+<div class="row">
+  <div class="col-md-9">
+    <div class="card">
+      <h2>My favourite salad this week</h2>
+      <h5>Categories: <strong style="color:cyan"> meat, kale, colourful</strong></h5>
+      
+  <div id="myCarousel" class="carousel slide" data-ride="carousel">
+    <!-- Indicators -->
+    <ol class="carousel-indicators">
+      <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+      <li data-target="#myCarousel" data-slide-to="1"></li>
+      <li data-target="#myCarousel" data-slide-to="2"></li>
+    </ol>
+
+    <!-- Wrapper for slides -->
+    <div class="carousel-inner">
+
+      <div class="item active">
+          <img src="green-salad.jpg" alt="Green Salad" style="width:100%;">
+        <div class="carousel-caption">
+          <h3>Green Salad</h3>
+          <p>This one is super tasty!</p>
+        </div>
+      </div>
+
+      <div class="item">
+          <img src="pastasalad.jpg" alt="Pasta Salad" style="width:100%;">
+        <div class="carousel-caption">
+          <h3>Pasta Salad</h3>
+          <p>Add pasta to your salad for a super filling meal!</p>
+        </div>
+      </div>
+    
+      <div class="item">
+          <img src="quinoasaladhero.jpg" alt="quinoa" style="width:100%;">
+        <div class="carousel-caption">
+          <h3>Quinoa Salad</h3>
+          <p>Add some quinoa for a vegetarian protein packed meal</p>
+        </div>
+      </div>
+  
+    </div>
+
+    <!-- Left and right controls -->
+    <a class="left carousel-control" href="#myCarousel" data-slide="prev">
+      <span class="glyphicon glyphicon-chevron-left"></span>
+      <span class="sr-only">Previous</span>
+    </a>
+    <a class="right carousel-control" href="#myCarousel" data-slide="next">
+      <span class="glyphicon glyphicon-chevron-right"></span>
+      <span class="sr-only">Next</span>
+    </a>
+  </div>
+</div>
+  </div>
+      <div class="col-md-3">
+    <div class="card">
+      <h2>Log In</h2>
+      <div class="login">
+          <form method="post">
+              <input type="text" id="username" name="username" placeholder="Username">
+              <input type="password" id="pwd" name="pwd" placeholder="Password">
+              <button type="submit" id="btn" name="submit">Login</button>
+                    
+          </form>
+      </div>
+    </div>
+    <div class="card">
+      <h3>Search by category</h3>
+      <ul>
+          <li><a style="color:cyan" href="#">Pasta salad</a></li>
+          <li><a style="color:cyan" href="#">Meaty salad</a></li>
+          <li><a style="color:cyan" href="#">Veggie salad</a></li>
+      </ul>
+    </div>
+    <div class="card">
+      <h3>Subscribe to my Newsletter</h3>
+      <p>Some text..</p>
+    </div>
+      </div>
+</div>
+      <div class="row">
+        <div class="col-md-3">
+    <div class="card">
+        <img src="salad2.jpg" alt="salad" class="card-img-top img-fluid" style="width:100%" />
+  <div class="card-block">
+    <h3 class="card-title">Headline</h3>
+    <p>Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+    <a href="#" class="btn btn-primary">Go somewhere</a>
+  </div>
+</div>
+      </div>
+        <div class="row">
+    <div class="col-md-3">
+    <div class="card">
+  <img src="salad2.jpg" alt="layered salad" class="card-img-top img-fluid" style="width:100%" />
+  <div class="card-block">
+    <h3 class="card-title">Headline</h3>
+    <p>Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+    <a href="#" class="btn btn-primary">Go somewhere</a>
+  </div>
+    </div>
+    
+        </div>
+         <div class="row">
+    <div class="col-md-3">
+    <div class="card">
+  <img src="salad2.jpg" alt="layered salad" class="card-img-top img-fluid" style="width:100%" />
+  <div class="card-block">
+    <h3 class="card-title">Headline</h3>
+    <p>Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+    <a href="#" class="btn btn-primary">Go somewhere</a>
+  </div>
+    </div>
+    </div>
+         </div>     
+  </div>
+      </div>
+      
+    
+  
+     
+
+<div class="footer">
+            
+                <div class="copywrite">
+                    &copy; <script>document.write(new Date().getFullYear())</script>, made with <i class="heart">&hearts;</i> by The Tech-sy Ladies
+             
+                </div>
+        </div>
+      </div>
+</div>
+   </body>
+</html>
